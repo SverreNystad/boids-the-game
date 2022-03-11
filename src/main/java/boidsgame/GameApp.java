fca04fde9fdf929c766a76a9da59b3e643869a18
Sverre Nystad <sverrnys@stud.ntnu.no>
2022-03-11 16:56:24 +0100

    Can see GUI (Appbar not supported), Made changes to fxml

--- src/main/java/boidsgame/GameApp.java
+++ src/main/java/boidsgame/GameApp.java
@@ -2,22 +2,28 @@ package boidsgame;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+// import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class GameApp extends Application {
-    
-    public static void main(String[] args) {
-        Application.launch(args);
-    }
-    // Problemet oppstå ved at jeg ikke har sagt i fmxlen at dette er controllerclassen    
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        primaryStage.setTitle("Boids the game");
-        primaryStage.setScene(new Scene(FXMLLoader.load(GameApp.class.getResource("mainMenu.fxml"))));
-        primaryStage.show();
-    }
-    
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+	// Problemet oppstå ved at jeg ikke har sagt i fmxlen at dette er controllerclassen    
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		primaryStage.setTitle("Boids the game");
+		primaryStage.setScene(new Scene(FXMLLoader.load(GameApp.class.getResource("mainMenu.fxml"))));
+		// primaryStage.setScene(new Scene(FXMLLoader.load(GameApp.class.getResource("settings.fxml"))));
+
+		primaryStage.show();
+	}
+
+		
+
+	
 }
--- src/main/java/boidsgame/GameController.java
+++ src/main/java/boidsgame/GameController.java
@@ -1,10 +1,46 @@
 package boidsgame;
 
+import java.io.IOException;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+// import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+
+// import javafx.scene.Scene;
+// import javafx.stage.Stage;
+// import javafx.scene.Node;
 
 public class GameController {
+	// private Stage stage; 
+	// private Scene scene;
+	@FXML
+	private Button playButton;
 	
+	@FXML
+	private Button settingsButton;
+
+	@FXML
+	private Button quitButton;
+	
+	
+	public void switchToMainMenu(ActionEvent event) throws IOException{
+	// 	// stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+	// 	scene = new Scene(FXMLLoader.load(GameApp.class.getResource("mainMenu.fxml")));
+	// 	stage.setScene(scene);
+	// 	stage.show();
+	}
+	public void switchToSettings(ActionEvent event) throws IOException{
+	// 	// stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+	// 	// stage = (Stage)(settingsButton.getSource()).getScrene().getWindow();
+
+	// 	scene = new Scene(FXMLLoader.load(GameApp.class.getResource("settings.fxml")));
+	// 	stage.setScene(scene);
+	// 	stage.show();
+	}
+
+	public void quitProgram(ActionEvent event) throws IOException{
+		Platform.exit();
+	}
 }
